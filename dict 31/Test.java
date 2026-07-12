0426a3d4aa984ffed173428df359f8081f992b80
Anirudh Ch <5220681mac@gmail.com>
2026-07-12 16:32:26 +0530

    Hello

---
+++ dict 31/Test.java
@@ -0,0 +1,10 @@
+public class  Test {
+    public static void main(String[] args) {
+        boolean a = true;
+        if(a = false){
+            System.out.println("a is true");
+        } else {
+            System.out.println("a is false");
+        }
+    }
+}
